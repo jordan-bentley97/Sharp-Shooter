185c0b0b03262556dc917ef8efb46a4e396d9fe2
J-Bentley <JordanBentley77@gmail.com>
2024-12-28 14:42:36 -0500

    added post processing
    BLOOM

--- Sharp-Shooter/Assets/Scripts/Enemies/EnemyHealth.cs
+++ Sharp-Shooter/Assets/Scripts/Enemies/EnemyHealth.cs
@@ -4,16 +4,24 @@ public class EnemyHealth : MonoBehaviour {
 
     [SerializeField] int startingHealth;
     [SerializeField] GameObject explosionVFX;
+
+    GameManager gameManager;
     int currentHealth;
 
     void Awake() {
         currentHealth = startingHealth;
     }
 
+    void Start() {
+        gameManager = FindFirstObjectByType<GameManager>();
+        gameManager.AdjustEnemiesRemaining(1); // will run when an enemy is instanced
+    }
+
     public void takeDamage(int amount){
         currentHealth -= amount;
 
         if (currentHealth <= 0){
+            gameManager.AdjustEnemiesRemaining(-1);
             SelfDestruct();
         }
     }
--- Sharp-Shooter/Assets/Scripts/Misc/GameManager.cs
+++ Sharp-Shooter/Assets/Scripts/Misc/GameManager.cs
@@ -1,7 +1,24 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
+
+    [SerializeField] TMP_Text enemiesRemainingText;
+    [SerializeField] GameObject youWinText;
+
+    int enemiesRemaining = 0;
+
+    const string ENEMIES_REMAINING_STRING = "Robots Remaining: ";
+
+    public void AdjustEnemiesRemaining(int amount) {
+        enemiesRemaining += amount;
+        enemiesRemainingText.text = ENEMIES_REMAINING_STRING + enemiesRemaining.ToString();
+
+        if (enemiesRemaining <= 0) {
+            youWinText.SetActive(true);
+        }
+    }
 
     public void ResetLevelButton() {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
